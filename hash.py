e3d56188e767d3805bd267535fc852c6d728b9ca
kondoutakahiro <s21013@std.it-college.ac.jpgit config --global user.name kondoutakahirogit commit -m 'my first commit'git config --global user.email s21013@std.it-college.ac.jp>
2021-06-09 08:02:03 +0000

    chap7

---
+++ hash.py
@@ -0,0 +1,17 @@
+tv =["ウオーキング,デット","アントラージュ","ザ．ソプラノズ","ヴァンパイア.ダイアリーズ"]
+for i, new in enumerate(tv):
+    new = tv[i]
+    new = new.upper()
+    tv[i] = new
+
+print(tv)
+
+
+for i in range(25,51):
+    print(i)
+
+
+show = ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"]
+for index, show in enumerate(shows):
+    print(index)
+    print(show)
